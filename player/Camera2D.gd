43dc708aaa5d47100f7ea3f5b7400e127f24de07
scalkin <nicklastandfest@gmail.com>
2023-10-23 13:44:38 -0500

    polished camera controls

--- player/Camera2D.gd
+++ player/Camera2D.gd
@@ -11,13 +11,21 @@ func _process(delta):
 		get_tree().paused = camera_mode
 		get_parent().visible = not camera_mode
 	
+	if Input.is_action_just_pressed("camera_reset"):
+		camera_mode = false
+		get_tree().paused = false
+		get_parent().visible = true
+		offset = Vector2.ZERO
+		zoom = Vector2.ONE
+	
 	if not camera_mode:
 		return
 	
-	offset += Input.get_vector("left", "right", "up", "down")
 	var zoom_input = Input.get_axis("zoom_out", "zoom_in")
 	
 	zoom_0 = clamp(zoom_0 + zoom_input*delta, 0.0, 1.0)
+	
+	offset += Input.get_vector("left", "right", "up", "down")*delta*(1.2 - zoom_0)*500
 	
 	zoom = Vector2(
 		zoom_curve.sample(zoom_0),
